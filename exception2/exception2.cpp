83154a9d9f11dd11ba9f3b81249e12706eb10ffd
ITaly177 Milan <patrokl2010@yandex.ru>
2023-02-15 16:48:34 +0300

    Add project files.

---
+++ exception2/exception2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+bool f(float x,float &result)
+{
+	if (x == 0) return false;
+	result =   1 / x;
+	return true;
+}
+
+int main(void)
+{
+	float x;
+	cout << "x? ";
+	cin >> x;
+	float x1;
+	if(f(x,x1))	cout << "1/x =" << x1 << endl;
+	else cout << "Division on zero exception" << endl;
+	
+
+	return 0;
+}
